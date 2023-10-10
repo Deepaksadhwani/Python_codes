e1b165d2093d3c5f8e1e58575333ce59174f8f20
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2023-10-11 02:46:17 +0530

    Update Prime_number_code.py

--- Prime_number_code.py
+++ Prime_number_code.py
@@ -1,15 +1,14 @@
 n = 10
 
-l1 = [True] * n
+l1 = [True] * (n + 1)  
 
-
-for i in range(2,int(n**0.5)):
+for i in range(2, int(n**0.5) + 1): 
     if l1[i]:
-        for j in range(i*i,n,i):
+        for j in range(i*i, n + 1, i): 
             l1[j] = False
 
-
-
-for i in range(2,len(l1)):
+for i in range(2, len(l1)):
     if l1[i]:
-        print(i, end= " ")
+        print(i, end=" ")
+
+
